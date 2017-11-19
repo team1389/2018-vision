ce6a978c94225d857c36b2fd10099a4d0fbc8a4f
An0rak <phos4es@gmail.com>
2017-11-19 15:41:26 -0500

    changed networktable reader to print values only when they have changed

--- caleb/caleb-networktable-reader.py
+++ caleb/caleb-networktable-reader.py
@@ -3,6 +3,14 @@ import time
 NetworkTables.initialize('192.168.1.6')
 sd = NetworkTables.getTable('MotionTracking')
 
+offsetX = str(sd.getNumber('offsetX', 'N/A'))
+offsetY = str(sd.getNumber('offsetY', 'N/A'))
+
 while(True):
-    print("offsetX: " + str(sd.getNumber('offsetX', 'N/A')))
-    print("offsetY: " + str(sd.getNumber('offsetY', 'N/A')))
+    newOffsetX = str(sd.getNumber('offsetX', 'N/A'))
+    newOffsetY = str(sd.getNumber('offsetY', 'N/A'))
+    if(offsetX != newOffsetX or offsetY != newOffsetY):
+        print("offsetX: " + newOffsetX)
+        print("offsetY: " + newOffsetY)
+        offsetX = newOffsetX
+        offsetY = newOffsetY
