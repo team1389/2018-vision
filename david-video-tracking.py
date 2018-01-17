ed6bc737086790c36a5d87642474cf35903a534f
David Swearingen <swearingendw@mac.com>
2018-01-17 17:11:50 -0500

    commenting

--- david-video-tracking.py
+++ david-video-tracking.py
@@ -1,5 +1,5 @@
 # Track a yellow cube and put a bounding box around it
-# Currently this is working on Mac MBP with its built-in camera
+# Currently, as of early Jan 2018, this is working on Mac MBP with its built-in camera
 # From https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_gui/py_video_display/py_video_display.html#display-video
 # press 'q' key to close video window when it's in the foreground
 
@@ -13,6 +13,9 @@ upper = np.array([93,255,255])
 
 # A kernal is like a matrix that is used in the morphology operation
 # See https://en.wikipedia.org/wiki/Kernel_(image_processing) if interested
+# It's important to understand the basics of this operation so that you know
+# what changing these values does.  These values should be experimented with
+# to optimize detection.  Values like (4,4) are a reasonable starting point.
 kernel = np.ones((6,6),np.uint8)
 
 cap = cv2.VideoCapture(0)
@@ -40,30 +43,29 @@ while(True):
     # It helps remove noise and jagged edges, note how the stray speckles are removed!
     opened = cv2.morphologyEx(res, cv2.MORPH_OPEN, kernel)
    
-   # Blurring operation helps forthcoming findContours operation work better
+    # Blurring operation helps forthcoming findContours operation work better
+    # The values here can be adjusted to tune the detection quality.  (3,3) is a decent starting point
     blur = cv2.blur(opened, (3,3))
     
     # Find contours
     (_, cnts, _) = cv2.findContours(blur.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)    
 
-    # Sometimes the contours operation will find more than two contours
-    # But if we did all our preliminary operations properly, then the two contours we need will be
-    # the *largest* contours in the set of contours.  The sorted operation below sorts
-    # the cnts array of contours by area, so the first two contours will be the largest
+    # Sometimes the contours operation will find more than one contour
+    # But if we did all our preliminary operations properly, then the contour we need will be
+    # the *largest* contour in the set of all contours.  The sorted operation below sorts
+    # the cnts array of contours by area, so the first contour should be the largest
   
     contours = sorted(cnts, key = cv2.contourArea, reverse = True)
-    
+
+    # If no contours are detected, then don't try to process them or you'll get an error:
     if len(contours) > 0:
         cnt1 = contours[0]
-        # cnt2 = sorted(cnts, key = cv2.contourArea, reverse = True)[1]
         
-        # Draw a minimum area rectangle around each contour
+        # Draw a minimum area rectangle around the contour
         rect1 = np.int32(cv2.boxPoints(cv2.minAreaRect(cnt1)))
-        # rect2 = np.int32(cv2.boxPoints(cv2.minAreaRect(cnt2)))
         
-        # Draw the contours in red (255, 0, 0) on top of our original image
+        # Draw the contour in red (255, 0, 0) on top of our original image
         cv2.drawContours(blur, [rect1], -1, (255, 0, 0), 2)
-        # cv2.drawContours(blur, [rect2], -1, (255, 0, 0), 2)    
     
     cv2.imshow('frame', blur)
 
